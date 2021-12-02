dfe81d3fbc1509eeaaa6c13057b3a8b3806ca2a6
jplayle <30301485+jplayle@users.noreply.github.com>
2021-12-02 15:57:43 +0000

    Update JP_d2.py

--- 2021/JP_d2.py
+++ 2021/JP_d2.py
@@ -1,12 +1,12 @@
 txt = """"""
 
 vals = {"forward": 0, "up": 0, "down": 0, "depth2": 0}
-fmap = {"forward": lambda n : (vals["up"] - vals["down"]) * n, "up": lambda n : 0, "down": lambda n : 0}
+fmap = {"forward": lambda n : (vals["down"] - vals["up"]) * n, "up": lambda n : 0, "down": lambda n : 0}
 
 for l in txt.split("\n"):
     ins, val = l.split(" ")[0], int(l.split(" ")[1])
     vals[ins] += val
     vals["depth2"] += fmap[ins](val)
     
-print(-vals["forward"] * (vals["up"] - vals["down"]))
-print(-vals["forward"] * vals["depth2"])
+print(vals["forward"] * (vals["down"] - vals["up"]))
+print(vals["forward"] * vals["depth2"])
