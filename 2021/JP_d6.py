351e2b832bf3866db11feaf62685c638781dcf39
jplayle <30301485+jplayle@users.noreply.github.com>
2021-12-06 12:05:50 +0000

    Update JP_d6.py

--- 2021/JP_d6.py
+++ 2021/JP_d6.py
@@ -3,13 +3,13 @@ lf_timers = "1,4,3,3,1,3,1,1,1,2,1,1,1,4,4,1,5,5,3,1,3,5,2,1,5,2,4,1,4,5,4,1,5,1
 lf_timers = [int(lft) for lft in lf_timers.split(",")]
 lf_timers = {x: lf_timers.count(x) for x in range(9)}
 
-prev_lft = lf_timers
-for n in range(256):
-    if n == 80:
-        print("Part 1 =", sum([y for x, y in prev_lft.items()]))
-    next_lft     = {x: prev_lft[x+1] for x in range(8)} 
-    next_lft[6] += prev_lft[0]
-    next_lft[8]  = prev_lft[0]
-    prev_lft     = next_lft
+def get_answer(days, prev_lft):
+    for n in range(days):
+        next_lft     = {x: prev_lft[x+1] for x in range(8)} 
+        next_lft[6] += prev_lft[0]
+        next_lft[8]  = prev_lft[0]
+        prev_lft     = next_lft
+    return sum([y for x, y in next_lft.items()])
 
-print("Part 2 =", sum([y for x, y in next_lft.items()]))
+print("Part 1 =", get_answer(80,  lf_timers))
+print("Part 2 =", get_answer(256, lf_timers))
