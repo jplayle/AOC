939ffe69d84adceda8570976bf2d47a2ca04968d
jplayle <30301485+jplayle@users.noreply.github.com>
2021-12-06 14:30:43 +0000

    Update JP_d6.py

--- 2021/JP_d6.py
+++ 2021/JP_d6.py
@@ -1,15 +1,15 @@
 lf_timers = "1,4,3,3,1,3,1,1,1,2,1,1,1,4,4,1,5,5,3,1,3,5,2,1,5,2,4,1,4,5,4,1,5,1,5,5,1,1,1,4,1,5,1,1,1,1,1,4,1,2,5,1,4,1,2,1,1,5,1,1,1,1,4,1,5,1,1,2,1,4,5,1,2,1,2,2,1,1,1,1,1,5,5,3,1,1,1,1,1,4,2,4,1,2,1,4,2,3,1,4,5,3,3,2,1,1,5,4,1,1,1,2,1,1,5,4,5,1,3,1,1,1,1,1,1,2,1,3,1,2,1,1,1,1,1,1,1,2,1,1,1,1,2,1,1,1,1,1,1,4,5,1,3,1,4,4,2,3,4,1,1,1,5,1,1,1,4,1,5,4,3,1,5,1,1,1,1,1,5,4,1,1,1,4,3,1,3,3,1,3,2,1,1,3,1,1,4,5,1,1,1,1,1,3,1,4,1,3,1,5,4,5,1,1,5,1,1,4,1,1,1,3,1,1,4,2,3,1,1,1,1,2,4,1,1,1,1,1,2,3,1,5,5,1,4,1,1,1,1,3,3,1,4,1,2,1,3,1,1,1,3,2,2,1,5,1,1,3,2,1,1,5,1,1,1,1,1,1,1,1,1,1,2,5,1,1,1,1,3,1,1,1,1,1,1,1,1,5,5,1"
 
 lf_timers = [int(lft) for lft in lf_timers.split(",")]
-lf_timers = {x: lf_timers.count(x) for x in range(9)}
+lf_timers = [lf_timers.count(x) for x in range(9)]
 
 def get_answer(days, prev_lft):
     for n in range(days):
-        next_lft     = {x: prev_lft[x+1] for x in range(8)} 
+        next_lft     = prev_lft[1:] + [0]
         next_lft[6] += prev_lft[0]
         next_lft[8]  = prev_lft[0]
         prev_lft     = next_lft
-    return sum([y for x, y in next_lft.items()])
+    return sum([x for x in next_lft])
 
 print("Part 1 =", get_answer(80,  lf_timers))
 print("Part 2 =", get_answer(256, lf_timers))
